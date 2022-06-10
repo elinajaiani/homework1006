78b85abb8c25de4b73995d9ebf0fa4e123befdbe
elina jaiani <104080573+elinajaiani@users.noreply.github.com>
2022-06-10 17:39:09 +0400

    count

--- homework1006.js
+++ homework1006.js
@@ -1,6 +1,19 @@
 var button = document.getElementById ("button");
 count = 10 ;
 button.onclick = function () {
-    count=count-1;
-    button=this.innerHTML="turn on" + count;
+  count=count-1;
+   button=this.innerHTML="click me" + count;
 }
+
+var button = document.getElementById ("button");
+count = 0 ;
+button.onclick = function () {
+  count=count+1;
+   button=this.innerHTML="click me" + count;
+}
+
+let number = 0;
+      document.querySelector("button").onclick = function() {
+          number = number + 1;
+          alert(number);
+      }
